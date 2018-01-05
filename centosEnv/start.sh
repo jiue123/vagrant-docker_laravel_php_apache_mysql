1a8fbec07b0a934cf77e529b37dadfbcd3e7145b
Le Thanh Phat <thanhphat.le@citynow.vn>
2018-01-05 11:56:42 +0700

    update code 05-01-2018

--- centosEnv/start.sh
+++ centosEnv/start.sh
@@ -1,19 +1,19 @@
 rm -rf /run/httpd/* /tmp/httpd*
 
 if grep -w "DB_HOST=127.0.0.1" $LARAVEL_ENVIRONMENT_FILE; then
-  sed -i -e 's/DB_HOST=127.0.0.1/DB_HOST=database/g' $LARAVEL_ENVIRONMENT_FILE
+  sed -i -e "s/DB_HOST=127.0.0.1/DB_HOST=$DB_HOST/g" $LARAVEL_ENVIRONMENT_FILE
 fi
 
 if grep -w "DB_DATABASE=homestead" $LARAVEL_ENVIRONMENT_FILE; then
-  sed -i -e 's/DB_DATABASE=homestead/DB_DATABASE=laravel/g' $LARAVEL_ENVIRONMENT_FILE
+  sed -i -e "s/DB_DATABASE=homestead/DB_DATABASE=$DB_DATABASE/g" $LARAVEL_ENVIRONMENT_FILE
 fi
 
 if grep -w "DB_USERNAME=homestead" $LARAVEL_ENVIRONMENT_FILE; then
-  sed -i -e 's/DB_USERNAME=homestead/DB_USERNAME=root/g' $LARAVEL_ENVIRONMENT_FILE
+  sed -i -e "s/DB_USERNAME=homestead/DB_USERNAME=$DB_USERNAME/g" $LARAVEL_ENVIRONMENT_FILE
 fi
 
 if grep -w "DB_PASSWORD=secret" $LARAVEL_ENVIRONMENT_FILE; then
-  sed -i -e 's/DB_PASSWORD=secret/DB_PASSWORD=123456/g' $LARAVEL_ENVIRONMENT_FILE
+  sed -i -e "s/DB_PASSWORD=secret/DB_PASSWORD=$DB_PASSWORD/g" $LARAVEL_ENVIRONMENT_FILE
 fi
 
 if php /var/www/html/artisan migrate; then
